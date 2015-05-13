7e2f40e17ffeecfcc251f0734dcc1aa683a9c1e6
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-13 18:59:57 +0400

    Создан файл для связи с базой данных.

---
+++ www/models/news.php
@@ -0,0 +1,8 @@
+<?php
+function News_getAll(){
+    return [
+        ['title' => 'post1', 'date' => '12.05.2015', 'text' => 'Some text about HTML'],
+        ['title' => 'post2', 'date' => '02.05.2015', 'text' => 'Some text about PHP'],
+        ['title' => 'post3', 'date' => '25.04.2015', 'text' => 'Some text about JS'],
+    ];
+}
