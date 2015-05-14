d9de124a4dd93cc27a8f4b26c23e07e6393b329c
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-14 20:35:14 +0400

    Подклчил модель news.php, к базе данных. Сделал выборку из таблицы в базе, отсортировав в обратном порядке по дате.

--- www/models/news.php
+++ www/models/news.php
@@ -1,8 +1,30 @@
 <?php
-function News_getAll(){
-    return [
-        ['title' => 'post1', 'date' => '12.05.2015', 'text' => 'Some text about HTML'],
-        ['title' => 'post2', 'date' => '02.05.2015', 'text' => 'Some text about PHP'],
-        ['title' => 'post3', 'date' => '25.04.2015', 'text' => 'Some text about JS'],
-    ];
-}
+    function News_getAll(){
+
+        $connection = mysqli_connect('localhost', 'root', '', 'test');
+        //Test if connection succeeded
+        if(mysqli_connect_errno()){
+            die("Database connection failed: "
+                . mysqli_connect_error() . " ("
+                . mysqli_connect_errno() . ")");
+        }
+
+        //Perform database query
+        $query  = "SELECT * ";
+        $query .= "FROM news ";
+        $query .= "ORDER BY date DESC";
+
+
+        $result = mysqli_query($connection, $query);
+        //Test if there was a query error
+        if(!$result){
+            die("Database query failed.");
+        }
+        $ret = [];
+
+        while ($row = mysqli_fetch_assoc($result)){
+            $ret[] = $row;
+        }
+
+        return $ret;
+    }
