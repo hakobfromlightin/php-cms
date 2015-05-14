aa101c04e37a32d5d91ac9fcaa67f35094156e4a
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-14 21:10:48 +0400

    Добавлены комментарии, и переведены на русский.

--- www/views/article.php
+++ www/views/article.php
@@ -1,4 +1,3 @@
-<?php $id = $_GET['id']; ?>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
@@ -6,38 +5,17 @@
 </head>
 <body>
     <h2>Article</h2>
-    <?php
-    //Perform database query
-    $query  = "SELECT name, text, date ";
-    $query .= "FROM news ";
-    $query .= "WHERE id =" . $id;
-
-    $result = mysqli_query($connection, $query);
-    //Test if there was a query error
-    if(!$result){
-        die("Database query failed.");
-    }
-
-    // Use returned data (if any)
-    if ($item = mysqli_fetch_assoc($result)){
-        //output data from each row
-        echo $item['name'];
-        echo $item['text'];
-        echo $item['date'];
-
-
-        $result = mysqli_query($connection, $query);
-        if(!$result){
-            die("Database query failed. " . mysqli_error($connection));
+    <?php foreach($items as $item): ?>
+        <?php
+        if (!empty($item["id"])) {
+            $article_id = $item["id"];
+        } else {
+            $article_id = 0;
         }
-    }
-    ?>
-
-    <?php// foreach($items as $item): ?>
-        <?php// echo $item['name']; ?><br>
-        <?php// echo $item['text']; ?><br>
-        <?php //echo $item['date']; ?><br>
-    <?php //endforeach; ?>
-
+        echo $item['name'] . '<br>';
+        echo $item['text'] . '<br>';
+        echo $item['date'] . '<br>';
+        ?><br>
+    <?php endforeach; ?>
 </body>
 </html>
