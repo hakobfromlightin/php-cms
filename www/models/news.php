2379d649e4117c402f888eee29a44430b3e57117
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-14 21:07:19 +0400

    Добавлена функция для отображения конкретной статьи.

--- www/models/news.php
+++ www/models/news.php
@@ -28,3 +28,32 @@
 
         return $ret;
     }
+
+    function News_get($page_id){
+        $connection = mysqli_connect('localhost', 'root', '', 'test');
+        //Test if connection succeeded
+        if(mysqli_connect_errno()){
+            die("Database connection failed: "
+                . mysqli_connect_error() . " ("
+                . mysqli_connect_errno() . ")");
+        }
+
+        //Perform database query
+        $query  = "SELECT name, text, date ";
+        $query .= "FROM news ";
+        $query .= "WHERE id =" . $page_id;
+
+
+        $result = mysqli_query($connection, $query);
+        //Test if there was a query error
+        if(!$result){
+            die("Database query failed.");
+        }
+        $ret = [];
+
+        while ($row = mysqli_fetch_assoc($result)){
+            $ret[] = $row;
+        }
+
+        return $ret;
+    }
