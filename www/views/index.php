0f09d9861239cb79e8df6ba324a2012be35f3f09
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-14 20:40:53 +0400

    Изменил вью для отображения всех новостией. Новости теперь в виде ссылок на кажду новость.

--- www/views/index.php
+++ www/views/index.php
@@ -5,8 +5,19 @@
     <title></title>
 </head>
 <body>
+    <h1>Blog</h1>
+    <h2>News</h2>
     <?php foreach($items as $item): ?>
-        <?php echo $item['title']; ?>
+        <?php
+        if (!empty($item["id"])) {
+            $article_id = $item["id"];
+        } else {
+            $article_id = 0;
+        }
+        $nws_ref = '<a href=article.php?id=' . $article_id . '>' . $item['name'] . '</a>';
+        echo $nws_ref;
+
+        ?><br>
     <?php endforeach; ?>
 </body>
 </html>
