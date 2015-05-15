4b5266b13a617cd691e71eee7c2fce456201b345
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-15 14:21:56 +0400

    Переписал функции модели news.php используя функции из sql.php

--- www/models/news.php
+++ www/models/news.php
@@ -1,59 +1,47 @@
 <?php
-    function News_getAll(){ // выводит все новости отсортированные по дате
 
-        $connection = mysqli_connect('localhost', 'root', '', 'test'); //подклчение к базе
-        // проверяем успешность подключения
-        if(mysqli_connect_errno()){
-            die("Database connection failed: "
-                . mysqli_connect_error() . " ("
-                . mysqli_connect_errno() . ")");
-        }
+    require_once __DIR__ . '/../functions/sql.php';
+
+    function News_getAll(){ // выводит все новости отсортированные по дате
 
         //выполняем запрос к базе
         $query  = "SELECT * ";
         $query .= "FROM news ";
         $query .= "ORDER BY date DESC";
 
-
-        $result = mysqli_query($connection, $query);
-        //проверяем не было ли ошибок
-        if(!$result){
-            die("Database query failed.");
-        }
-        $ret = [];
-
-        while ($row = mysqli_fetch_assoc($result)){
-            $ret[] = $row;
-        }
-
-        return $ret;
+        return sql_query($query); //подключение к базе и выполнение запроса
     }
 
     function News_get($page_id){ // выводит выбранную пользователем новость
-        $connection = mysqli_connect('localhost', 'root', '', 'test');//подклчение к базе
-        // проверяем успешность подключения
-        if(mysqli_connect_errno()){
-            die("Database connection failed: "
-                . mysqli_connect_error() . " ("
-                . mysqli_connect_errno() . ")");
-        }
 
         //выполняем запрос к базе
         $query  = "SELECT name, text, date ";
         $query .= "FROM news ";
         $query .= "WHERE id =" . $page_id;
 
+        return sql_query($query); //выполнение запроса
+    }
 
-        $result = mysqli_query($connection, $query);
-        //проверяем не было ли ошибок
-        if(!$result){
-            die("Database query failed.");
+    function News_insert(){ // добавляет новость в базу данных
+
+        if(empty($_POST['name']) || empty($_POST['text'])){ //проверяем введены ли данные
+            echo "Название или текст пустые";
+            exit;
+        } else {
+            $name = $_POST['name'];
+            $text = $_POST['text'];
+            $date = date("Y.d.m");
+
+            // команда вставки в таблицу
+            $query = "INSERT INTO news (";
+            $query .= "name, text, date";
+            $query .= ")VALUES (";
+            $query .= " '{$name}', '{$text}', '{$date}'";
+            $query .= ")";
+
+            $res = sql_query($query);
+            if($res){
+                return true;
+            }
         }
-        $ret = [];
-
-        while ($row = mysqli_fetch_assoc($result)){
-            $ret[] = $row;
-        }
-
-        return $ret;
     }
