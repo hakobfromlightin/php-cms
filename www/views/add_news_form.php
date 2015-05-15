4965f4d36d2ea3445914f4a318fc01726f24fd50
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-15 15:30:28 +0400

    Создал форму добавления статьи.

---
+++ www/views/add_news_form.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+       <h1>Add an article</h1>
+    <form action="/add_news.php" method="post">
+        Article name:<input type="text" name="title"><br>
+        Text:<input type="text" name="article"><br>
+        <input type="submit" value="Отправить"><br>
+    </form>
+
+<a href="../index.php">Home</a><br/>
+</body>
+</html>
