977ea44a6a8f14ee96424d3c5804aefa5540c912
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-21 20:35:08 +0400

    Добавил несколько <div> элементов.

--- www/views/index.php
+++ www/views/index.php
@@ -5,27 +5,35 @@
     <title></title>
 </head>
 <body>
-    <h1>Blog</h1>
-    <h2>News</h2>
-    <table border="1">
-        <tr>
-            <td>Название</td>
-        </tr>
-        <?php foreach($items as $item): ?>
-        <tr>
-            <td><?php
-            if (!empty($item["id"])) {
-                $article_id = $item["id"];
-            } else {
-                $article_id = 0;
-            }
-            $nws_ref = '<a href=article.php?id=' . $article_id . '>' . $item['name'] . '</a>';
-            echo $nws_ref;
-            ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
+    <div class="header">
+        <h1>Blog</h1>
+    </div>
+    <div class="content">
+        <h2>News</h2>
+        <table border="1">
+            <tr>
+                <td>Название</td>
+            </tr>
+            <?php foreach($items as $item): ?>
+            <tr>
+                <td><?php
+                if (!empty($item["id"])) {
+                    $article_id = $item["id"];
+                } else {
+                    $article_id = 0;
+                }
+                $nws_ref = '<a href=article.php?id=' . $article_id . '>' . $item['name'] . '</a>';
+                echo $nws_ref;
+                ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
 
-    <a href="../add_news.php">Add an article</a>
+        <a href="../add_news.php">Add an article</a>
+    </div>
+    <div class="footer">
+
+    </div>
 </body>
+
 </html>
