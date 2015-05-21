3817a637d825c22bf9c00b4002f23a831d739b4c
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-05-21 20:52:06 +0400

    Добавил ссылку на редактирование новости.

--- www/views/article.php
+++ www/views/article.php
@@ -4,19 +4,20 @@
     <title></title>
 </head>
 <body>
-    <h2>Article</h2>
-    <?php foreach($items as $item): ?>
-        <?php
-        if (!empty($item["id"])) {
-            $article_id = $item["id"];
-        } else {
-            $article_id = 0;
-        }
-        echo $item['name'] . '<br>';
-        echo $item['text'] . '<br>';
-        echo $item['date'] . '<br>';
-        ?><br>
-    <?php endforeach; ?>
-    <a href="../add_news.php">Add an article</a>
+<h2>Article</h2>
+<?php foreach ($items as $item): ?>
+    <?php
+    if (!empty($item["id"])) {
+        $article_id = $item["id"];
+    } else {
+        $article_id = 0;
+    }
+    echo $item['name'] . '<br>';
+    echo $item['text'] . '<br>';
+    echo $item['date'] . '<br>';
+    ?><br>
+<?php endforeach; ?>
+<a href="../update_news.php">Update an article</a><br>
+<a href="../add_news.php">Add an article</a><br>
 </body>
 </html>
