2a59bfa8bd8f1e72ddc9b18965a7a31a3f064108
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-06-28 14:32:30 +0400

    Вернул методы которые были в абстрактном классе

--- www/models/NewsModel.php
+++ www/models/NewsModel.php
@@ -10,4 +10,42 @@ class NewsModel
     protected static $table = 'news';
     protected static $order = 'date DESC';
     protected static $class = 'NewsModel';
+
+    public static function insert($data)
+    { // добавляет новость в базу данных
+        $name = $data['title'];
+        $text = $data['article'];
+        $date = date("Y-m-d");
+
+        $database = new Database();
+
+        // команда вставки в таблицу
+        $query = "INSERT INTO news (";
+        $query .= "name, text, date";
+        $query .= ")VALUES (";
+        $query .= " '$name', '$text', '$date'";
+        $query .= ")";
+
+        return $database->queryAll($query, static::$class);
+    }
+
+    /*
+    public static function update($data)
+    {// обновляем новость в базе
+       $name = $data['title'];
+       $text = $data['article'];
+
+       $database = new Database();
+
+       // команда обновления таблицы
+       $query = "UPDATE news SET ";
+       $query .= "name = '{$name}', ";
+       $query .= "text = {$text} ";
+       $query .= "WHERE id = {$_GET['id']}";
+
+       $res = $database->queryAll($query);
+
+       return $res;
+    }
+    */
 }
