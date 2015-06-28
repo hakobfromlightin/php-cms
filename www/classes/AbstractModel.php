70535c7a56975cce9542749a853f1cc82eff50a5
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-06-28 14:34:38 +0400

    Создал абстрактную модель.

---
+++ www/classes/AbstractModel.php
@@ -0,0 +1,35 @@
+<?php
+
+abstract class AbstractModel
+{
+
+    protected static $table;
+    protected static $order;
+    protected static $class;
+
+    public static function getAll()
+    { // метод выводит все новости отсортированные по дате
+
+        $database = new Database();
+
+        //выполняем запрос к базе
+        $query = "SELECT * ";
+        $query .= "FROM " . static::$table;
+        $query .= " ORDER BY " . static::$order;
+        return $database->queryAll($query, static::$class); //подключение к базе и выполнение запроса
+    }
+
+    public static function getOne($id)
+    { //выводит выбранную пользователем новость
+
+        $database = new Database();
+
+        //выполняем запрос к базе
+        $query = "SELECT name, text, date ";
+        $query .= "FROM " . static::$table;
+        $query .= " WHERE id =" . $id;
+
+        return $database->queryOne($query, static::$class); //выполнение запроса
+    }
+
+}
