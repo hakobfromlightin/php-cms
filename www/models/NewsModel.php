e6d14da0b20ddf171f5f7cffbc99cdf723148e17
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-07-14 23:41:37 +0400

    ORM

--- www/models/NewsModel.php
+++ www/models/NewsModel.php
@@ -4,26 +4,4 @@ class NewsModel
     extends AbstractModel
 {
     protected static $table = 'news';
-
-    /*
-
-
-    public static function update($data)
-    {// обновляем новость в базе
-       $name = $data['title'];
-       $text = $data['article'];
-
-       $database = new Database();
-
-       // команда обновления таблицы
-       $query = "UPDATE news SET ";
-       $query .= "name = '{$name}', ";
-       $query .= "text = {$text} ";
-       $query .= "WHERE id = {$_GET['id']}";
-
-       $res = $database->queryAll($query);
-
-       return $res;
-    }
-    */
 }
